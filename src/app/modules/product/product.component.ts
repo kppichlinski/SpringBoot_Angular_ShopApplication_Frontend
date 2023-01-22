36f40bd1cfc4eae6f9c8ed709ff365e528830a05
kpichlinski <kppichlinski@gmail.com>
2023-01-22 14:11:18 +0100

    getting data from backend

--- src/app/modules/product/product.component.ts
+++ src/app/modules/product/product.component.ts
@@ -18,6 +18,7 @@ export class ProductComponent {
   }
 
   getProducts() {
-    this.products = this.productService.getProducts();
+    this.productService.getProducts()
+      .subscribe(products => this.products = products);
   }
 }
