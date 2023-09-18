7e64c749a8064d869cc32e3fe7fb1fd9f9467444
kpichlinski <kppichlinski@gmail.com>
2023-09-18 17:23:34 +0200

    status change

--- src/app/modules/admin/admin-order/admin-order-update/admin-order-update.component.ts
+++ src/app/modules/admin/admin-order/admin-order-update/admin-order-update.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { AdminOrderService } from '../admin-order.service';
 import { AdminOrder } from '../model/adminOrder';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-admin-order-update',
@@ -10,20 +11,44 @@ import { AdminOrder } from '../model/adminOrder';
 })
 export class AdminOrderUpdateComponent implements OnInit {
 
-  order!: AdminOrder;
+  order: AdminOrder | undefined;
+  formGroup!: FormGroup;
+  statuses!: Map<string, string>;
 
   constructor(
     private activatedRoute: ActivatedRoute,
-    private adminOrderService: AdminOrderService
-    ) { }
+    private adminOrderService: AdminOrderService,
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit(): void {
+    this.getInitData();
     this.getOrder();
+    this.formGroup = this.formBuilder.group({
+      orderStatus: ['', Validators.required]
+    });
   }
 
   getOrder() {
     let id = Number(this.activatedRoute.snapshot.params['id']);
     this.adminOrderService.getOrder(id)
-      .subscribe(order => this.order = order);
+      .subscribe(order => {
+        this.order = order;
+        this.formGroup.setValue({
+          orderStatus: order.orderStatus
+        });
+      });
+  }
+
+  changeStatus() {
+    this.adminOrderService.saveStatus(this.order?.id!, this.formGroup.value)
+      .subscribe();
+  }
+
+  getInitData() {
+    this.adminOrderService.getInitData()
+      .subscribe(data => {
+        this.statuses = data.orderStatuses;
+      });
   }
 }
